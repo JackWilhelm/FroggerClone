da03cf25c2ea43fd320e818b22f2f429fd2785ac
JackWilhelm <jgwilhel@ucsc.edu>
2025-01-15 15:48:46 -0800

    Green circle moves with WASD

--- src/Scenes/Frogger.js
+++ src/Scenes/Frogger.js
@@ -9,10 +9,25 @@ class Frogger extends Phaser.Scene {
     }
 
     create() {  
-
+        this.my.sprite.frog = this.add.ellipse(config.width/2, config.height/2, 50, 50, 0x8000);
+        this.downKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
     
     update() {
-
+        if (this.downKey.isDown) {
+            this.my.sprite.frog.y += 5;
+        }
+        if (this.upKey.isDown) {
+            this.my.sprite.frog.y -= 5;
+        }
+        if (this.leftKey.isDown) {
+            this.my.sprite.frog.x -= 5;
+        }
+        if (this.rightKey.isDown) {
+            this.my.sprite.frog.x += 5;
+        }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 "use strict"
 
 let config = {
-    parent: 'phaser-game',
     type: Phaser.CANVAS,
     render: {
         pixelArt: true
     },
     width: 1000,
     height: 800,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:300},
+            debug: false
+        }
+    },
     scene: [Frogger],
     fps: { forceSetTimeOut: true, target: 30 }
 }
-
-var my = {sprite: {}};
 
 const game = new Phaser.Game(config);
